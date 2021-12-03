55cbf549cedd9cb0b29d842d1a754d44c22d9bda
Moussa FOFANA <didegassama@gmail/com>
2021-12-03 18:22:25 +0000

    Premier commit

---
+++ app/Http/Controllers/PlatController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Plat;
+use Illuminate\Http\Request;
+
+class PlatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Plat  $plat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Plat $plat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Plat  $plat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Plat $plat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Plat  $plat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Plat $plat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Plat  $plat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Plat $plat)
+    {
+        //
+    }
+}
